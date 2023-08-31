7d6f93747bf37f06667c03957617c83571159bea
Tadek Waluga <tadek.waluga@gmail.com>
2023-08-31 22:15:13 +0200

    updated styled, added style for Rating

--- src/common/MovieTile/styled.js
+++ src/common/MovieTile/styled.js
@@ -12,6 +12,7 @@ export const Wrapper = styled.div`
 `;
 
 export const Poster = styled.img`
+  display: block;
   width: 292px;
   height: 434px;
   margin-top: 16px;
@@ -20,27 +21,21 @@ export const Poster = styled.img`
 
 export const Description = styled.div`
   width: 292px;
-  height: 105px;
 `;
 
 export const Title = styled.div`
   width: 292px;
-  height: 29px;
-  font-family: Poppins;
   font-size: 22px;
   font-weight: 500;
   line-height: 1.3;
-  letter-spacing: 0px;
   text-align: left;
-  color: black;
+  margin: 8px 0;
 `;
 
 export const Year = styled.div`
   font-size: 16px;
   font-weight: 400;
   line-height: 1.5;
-  letter-spacing: 0px;
-  text-align: left;
   color: grey;
 `;
 
@@ -48,13 +43,27 @@ export const Tags = styled.div`
   width: 279px;
   height: 36px;
   display: flex;
+  flex-wrap: wrap;
   gap: 8px;
+  margin: 8px 0;
 `;
 
 export const Tag = styled.div`
   height: 36px;
   padding: 8px 16px 8px 16px;
   border-radius: 5px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  text-align: left;
   background-color: #E4E6F0;
-;
+`;
+
+export const Rating = styled.div`
+  width: 140px;
+  height: 24px;
+  top: 610px;
+  left: 16px;
+  gap: 12px;
+
 `;
