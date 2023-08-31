062baf1a3f81f7b5e378de12fc59bfcfc3cdb919
Piotr Michalski <pipi6620@gmail.com>
2023-08-31 20:55:23 +0200

    displaying ErrorMessage in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Navigation } from "./common/Navigation";
 import { Pagination } from "./common/Pagination";
 import { PersonTile } from "./common/PersonTile";
+import { ErrorMessage } from "./common/States/Error";
 import { LoadingIcon } from "./common/States/Loading";
 import { NoResults } from "./common/States/NoResults";
 
@@ -10,6 +11,7 @@ function App() {
       <Navigation />
       <LoadingIcon />
       <NoResults />
+      <ErrorMessage />
       <PersonTile />
       <Pagination />
     </>
