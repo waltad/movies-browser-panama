79ed717e6571133eef3d9715832c0bfa7fa75f52
Piotr Michalski <pipi6620@gmail.com>
2023-08-31 20:09:11 +0200

    displaying NoResults component in App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Navigation } from "./common/Navigation";
 import { Pagination } from "./common/Pagination";
 import { PersonTile } from "./common/PersonTile";
 import { LoadingIcon } from "./common/States/Loading";
+import { NoResults } from "./common/States/NoResults";
 
 function App() {
   return (
     <>
       <Navigation />
       <LoadingIcon />
+      <NoResults />
       <PersonTile />
       <Pagination />
     </>
