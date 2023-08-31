283f0f379b744cba3e7d2be5256a15c6fb722720
Piotr Michalski <pipi6620@gmail.com>
2023-08-31 19:56:40 +0200

    displaying loading spinner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Navigation } from "./common/Navigation";
 import { Pagination } from "./common/Pagination";
 import { PersonTile } from "./common/PersonTile";
+import { LoadingIcon } from "./common/States/Loading";
 
 function App() {
   return (
     <>
       <Navigation />
+      <LoadingIcon />
       <PersonTile />
       <Pagination />
     </>
