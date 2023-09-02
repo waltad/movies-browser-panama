c563ed91adb43228a8a9a94d068166b6bc010222
Piotr Michalski <pipi6620@gmail.com>
2023-09-02 23:58:47 +0200

    Error responsiveness added

--- src/common/States/Error/styled.js
+++ src/common/States/Error/styled.js
@@ -3,6 +3,16 @@ import { ReactComponent as Error } from "../../../images/Error.svg";
 
 export const ErrorIcon = styled(Error)`
     margin-top: 180px;
+    height: auto;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        margin-top: 80px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+        margin-top: 40px;
+        width: 60px;
+    }
 `;
 
 export const Heading = styled.h1`
@@ -10,6 +20,14 @@ export const Heading = styled.h1`
     font-size: 36px;
     line-height: 1.2;
     margin: 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        font-size: 26px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+        font-size: 20px;
+    }
 `;
 
 export const Subheading = styled.h2`
@@ -17,6 +35,14 @@ export const Subheading = styled.h2`
     font-size: 22px;
     line-height: 1.3;
     margin: 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        font-size: 16px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+        font-size: 12px;
+    }
 `;
 
 export const Button = styled.a`
@@ -30,4 +56,9 @@ export const Button = styled.a`
     font-size: 14px;
     line-height: 1.35;
     cursor: pointer;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+        font-size: 10px;
+        padding: 12px 18px;
+    }
 `;
