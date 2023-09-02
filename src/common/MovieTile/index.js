a4f6844361b7cb5ed139e4218686746eb5b82220
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-02 20:56:21 +0200

    Added 2 divs, changed Title to Genre

--- src/common/MovieTile/index.js
+++ src/common/MovieTile/index.js
@@ -1,25 +1,30 @@
-import { Description, Poster, Wrapper, Title, Year, Tags, Tag, Rating, Star, Value, Votes } from "./styled";
+import { Poster, Wrapper, Title, Year, Genres, Genre, Rating, Star, Value, Votes } from "./styled";
 import noPoster from "../../images/Movie.png";
 import star from "../../images/Star.png";
 
 export const MovieTitle = () => {
   return (
     <Wrapper>
-      <Poster src={noPoster} alt="" />
-      <Description>
-        <Title>Mulan long title long title Mulan long title long</Title>
-        <Year>2020</Year>
-        <Tags>
-          <Tag>Action</Tag>
-          <Tag>Adventure</Tag>
-          <Tag>Drama</Tag>
-        </Tags>
-      </Description>
+      <div>
+        <Poster src={noPoster} alt="" />
+
+        <div>
+          <Title>Mulan long title long title Mulan long title long </Title>
+          <Year>2020</Year>
+          <Genres>
+            <Genre>Action</Genre>
+            <Genre>Adventure</Genre>
+            <Genre>Drama</Genre>
+          </Genres>
+        </div>
+      </div>
+
       <Rating>
-        <Star src={star} alt="star"/>
+        <Star src={star} alt="star" />
         <Value>7.8</Value>
         <Votes>35 Votes</Votes>
       </Rating>
+
     </Wrapper>
   )
 };
