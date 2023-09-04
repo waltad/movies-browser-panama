70d57418ef27b551f82de2b68a77a126d5114db1
Piotr Michalski <pipi6620@gmail.com>
2023-09-04 23:03:24 +0200

    created store

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import moviesReducer from "./features/moviesSlice";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+    reducer: {
+        movies: moviesReducer,
+    },
+    middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
