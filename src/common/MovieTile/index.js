ecd3a4066422598eaabaf0ec64e073bfc93d1dfc
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-09-04 11:33:37 +0200

    Added MainInfo component, updated code formating

--- src/common/MovieTile/index.js
+++ src/common/MovieTile/index.js
@@ -1,30 +1,39 @@
-import { Poster, Wrapper, Title, Year, Genres, Genre, Rating, Star, Value, Votes } from "./styled";
+import {
+  Poster,
+  MainInfo,
+  Wrapper,
+  Title,
+  Year,
+  Genres,
+  Genre,
+  Rating,
+  Star,
+  Value,
+  Votes,
+} from "./styled";
 import noPoster from "../../images/Movie.png";
 import star from "../../images/Star.png";
 
 export const MovieTitle = () => {
   return (
     <Wrapper>
-      <div>
-        <Poster src={noPoster} alt="" />
+      <Poster src={noPoster} alt="" />
 
-        <div>
-          <Title>Mulan long title long title Mulan long title long </Title>
-          <Year>2020</Year>
-          <Genres>
-            <Genre>Action</Genre>
-            <Genre>Adventure</Genre>
-            <Genre>Drama</Genre>
-          </Genres>
-        </div>
-      </div>
+      <MainInfo>
+        <Title>Mulan longer tite longer </Title>
+        <Year>2020</Year>
+        <Genres>
+          <Genre>Action</Genre>
+          <Genre>Adventure</Genre>
+          <Genre>Drama</Genre>
+        </Genres>
+      </MainInfo>
 
       <Rating>
         <Star src={star} alt="star" />
         <Value>7.8</Value>
         <Votes>35 Votes</Votes>
       </Rating>
-
     </Wrapper>
-  )
+  );
 };
