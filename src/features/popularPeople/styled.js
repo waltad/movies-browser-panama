9514aecd44014cc51fbce2117c00ed6f635ea9a9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-09-05 22:35:38 +0100

    popularPeople @media added

--- src/features/popularPeople/styled.js
+++ src/features/popularPeople/styled.js
@@ -3,8 +3,11 @@ import { NavLink } from "react-router-dom";
 
 export const Wrapper = styled.div`
   max-width: 1368px;
-  margin-bottom: 100px;
-  margin: 56px auto 40px;
+  margin: 56px auto 123px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    margin: 24px 16px 0px 16px;
+  }
 `;
 
 export const Header = styled.h1`
@@ -13,12 +16,29 @@ export const Header = styled.h1`
   line-height: 1.2;
   color: ${({ theme }) => theme.colors.woodSmoke};
   margin-left: 10px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 20px;
+  }
 `;
 
 export const PeopleList = styled.ul`
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(195px, 1fr));
-    gap: 24px;
-    padding: 12px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(195px, 1fr));
+  gap: 24px;
+  padding: 12px 12px 16px 12px;
+  margin-bottom: 16px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+    height: 100%;
+  }
 `;
+
+
+
 
