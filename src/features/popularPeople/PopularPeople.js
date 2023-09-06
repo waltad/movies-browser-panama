be8a852d735c129a26cae76140b114890503b038
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-09-06 23:47:26 +0100

    imports and new path added

--- src/features/popularPeople/PopularPeople.js
+++ src/features/popularPeople/PopularPeople.js
@@ -1,6 +1,9 @@
 import { Wrapper, Header, PeopleList, StyledNavLink } from "./styled";
 import { PersonTile } from "../../common/PersonTile";
 import { Pagination } from "../../common/Pagination";
+import { ErrorMessage } from "../../common/States/Error";
+import { LoadingIcon } from "../../common/States/Loading";
+import { NoResults } from "../../common/States/NoResults";
 
 const PopularPeople = () => {
   return (
@@ -8,7 +11,7 @@ const PopularPeople = () => {
       <Wrapper>
         <Header>Popular people</Header>
         <PeopleList>
-          <StyledNavLink to="/default-path">
+          <StyledNavLink to="/personDetails">
             <PersonTile />
           </StyledNavLink>
         </PeopleList>
