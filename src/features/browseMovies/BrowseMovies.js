8062ef10a884b765c825c4d6457da77b21b4c2a7
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-09-06 14:36:51 +0200

    added PopularMovies to BrowserMovies

--- src/features/browseMovies/BrowseMovies.js
+++ src/features/browseMovies/BrowseMovies.js
@@ -1,15 +1,15 @@
-import { MovieTitle } from "../../common/MovieTile";
+// import { MovieTile } from "../../common/MovieTile";
 import { Pagination } from "../../common/Pagination";
 import { PersonTile } from "../../common/PersonTile";
 import { ErrorMessage } from "../../common/States/Error";
 import { LoadingIcon } from "../../common/States/Loading";
 import { NoResults } from "../../common/States/NoResults";
+import PopularMovies from "../popularMovies";
 
 const BrowseMovies = () => {
   return (
     <>
-      <p>Browse Movies (NYI)</p>
-      <MovieTitle />
+      <PopularMovies />
       <LoadingIcon />
       <NoResults />
       <ErrorMessage />
