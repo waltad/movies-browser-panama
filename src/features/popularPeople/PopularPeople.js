3b081d4989c5c25c1fdd73daf7460a5a64b64bb3
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-09-08 00:13:55 +0100

    Content to popularPeople added

--- src/features/popularPeople/PopularPeople.js
+++ src/features/popularPeople/PopularPeople.js
@@ -1,9 +1,7 @@
 import { Wrapper, Header, PeopleList, StyledNavLink } from "./styled";
 import { PersonTile } from "../../common/PersonTile";
 import { Pagination } from "../../common/Pagination";
-import { ErrorMessage } from "../../common/States/Error";
-import { LoadingIcon } from "../../common/States/Loading";
-import { NoResults } from "../../common/States/NoResults";
+import Content from "./Content";
 
 const PopularPeople = () => {
   return (
