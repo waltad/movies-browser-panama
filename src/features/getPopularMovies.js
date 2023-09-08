4e2d94ea692f6befc20a0cdc7f1be7689c44883e
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-08 22:05:33 +0200

    Added dispatch to PopularMovies

--- src/features/getPopularMovies.js
+++ src/features/getPopularMovies.js
@@ -13,8 +13,8 @@ export const getPopularMovies = async () => {
   const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options);
 
   if (!response.ok) {
-   new Error(response.statusText);
+    new Error(response.statusText);
   }
-  
+
   return await response.json();
 };
--- src/features/popularMovies/index.js
+++ src/features/popularMovies/index.js
@@ -1,9 +1,16 @@
 // import { Navigation } from "../../common/Navigation";
 // import { Pagination } from "../../common/Pagination";
+import { useDispatch } from "react-redux";
 import { MovieTile } from "../../common/MovieTile";
 import { Container, Title, Wrapped } from "./styled";
+import { fetchPopularMovies } from "../moviesSlice";
 
 const PopularMovies = () => {
+  
+  const dispatch = useDispatch();
+  
+  dispatch(fetchPopularMovies());
+
   return (
     <>
       <Wrapped>
