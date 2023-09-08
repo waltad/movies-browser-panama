3fd40d5a72fb225a30d063c7d6ef361432881c4e
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-08 19:09:33 +0200

    updated getPolularMovies function

--- src/features/getPopularMovies.js
+++ src/features/getPopularMovies.js
@@ -1,14 +1,20 @@
-export const getPopularMovies = () => {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZWQ5MDdhYjMxZTkwYjJjYTY4MTVhMDAyYmRkMWE3YyIsInN1YiI6IjY0ZjlhNjFhNWYyYjhkMDEzOWQwYTgwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PS_mo30389N144N9mkpPelkWgOM616tY1cH5wbzpGC4'
-    }
-  };
+import { authorizationKey } from "./authorizationKey";
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: authorizationKey
+  }
+};
+
+export const getPopularMovies = async () => {
+
+  const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options);
+
+  if (!response.ok) {
+   new Error(response.statusText);
+  }
   
-  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  return await response.json();
 };
