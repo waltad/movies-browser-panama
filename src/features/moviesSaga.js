6ecb23ff3f70abc8782af3f4727297e9e245eddf
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-10 18:57:32 +0200

    updated moviesSaga popularMovies and genres

--- src/features/moviesSaga.js
+++ src/features/moviesSaga.js
@@ -1,13 +1,18 @@
 import { takeEvery, call, put, throttle } from "redux-saga/effects";
 import { fetchPopularMovies, fetchPopularMoviesError, setGenres, setMoviesData } from "./moviesSlice";
+import { getPopularMovies } from "./getPopularMovies";
+import { getGenresList } from "./getGenresList";
 
 function* fetchPopularMoviesHandler() {
     try {
         yield throttle(1000);
+        const popularMovies = yield call(getPopularMovies);
+        const genres = yield call(getGenresList);
         // const moviesData = yield call(getMoviesData);
         // const genres = yield call(getGenres);
         // yield put(setGenres(genres));
-        // yield put(setMoviesData(moviesData));
+        yield put(setMoviesData(popularMovies.results));
+        yield put(setGenres(genres.genres));
     } catch (error) {
         yield put(fetchPopularMoviesError());
     }
