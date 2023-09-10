27b18ff6d70c87e6e0ab7d55047cf89b44f87f1d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-09-10 18:11:19 +0100

    getPeople.js added

---
+++ src/features/getPeople.js
@@ -0,0 +1,16 @@
+import { APIkey, peopleUrl } from "../apiURL";
+
+export const getPeople = async (page) => {
+  try {
+    const response = await fetch(
+      `${peopleUrl}?api_key=${APIkey}&language=en-US&page=${page}`
+    );
+    if (!response.ok) {
+      throw new Error("An error occurred. Please try again later.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
