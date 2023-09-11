955540375bd9c6e8b36478e63fa56332d6defdb0
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-09-11 13:40:26 +0200

    Added error, loadding components and selectors

--- src/features/popularMovies/index.js
+++ src/features/popularMovies/index.js
@@ -1,33 +1,55 @@
-// import { Navigation } from "../../common/Navigation";
-// import { Pagination } from "../../common/Pagination";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  selectStatus,
+  selectData,
+  selectGenres
+} from "../moviesSlice";
 import { MovieTile } from "../../common/MovieTile";
+import { ErrorMessage } from "../../common/States/Error";
+import { LoadingIcon } from "../../common/States/Loading";
 import { Container, Title, Wrapped } from "./styled";
 import { fetchPopularMovies } from "../moviesSlice";
 
 const PopularMovies = () => {
-  
   const dispatch = useDispatch();
-  
-  dispatch(fetchPopularMovies());
+  const location = useLocation().pathname;
+  const status = useSelector(selectStatus);
+  const popularMoviesList = useSelector(selectData);
+  const genres = useSelector(selectGenres);
 
-  return (
-    <>
-      <Wrapped>
-        <Title>Popular Movies</Title>
-        <Container>
-          <MovieTile />
-          <MovieTile />
-          <MovieTile />
-          <MovieTile />
-          <MovieTile />
-          <MovieTile />
-          <MovieTile />
-          <MovieTile />
-        </Container>
-      </Wrapped>
-    </>
-  );
+  useEffect(() => {
+    if (location.includes("movie")) dispatch(fetchPopularMovies());
+  }, []);
+
+  if (status === "error") {
+    return <ErrorMessage />;
+  }
+
+  if (status === "loading") {
+    return <LoadingIcon />;
+  }
+
+  if (status === "success") {
+    return (
+      <>
+        <Wrapped>
+          <Title>Popular Movies</Title>
+          <Container>
+            <MovieTile />
+            <MovieTile />
+            <MovieTile />
+            <MovieTile />
+            <MovieTile />
+            <MovieTile />
+            <MovieTile />
+            <MovieTile />
+          </Container>
+        </Wrapped>
+      </>
+    );
+  }
 };
 
 export default PopularMovies;
