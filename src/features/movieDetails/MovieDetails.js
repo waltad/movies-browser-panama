1ab5e57c48f938be4bf3c8c1c3f681173c363f02
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-12 21:41:57 +0200

    added first styled to MovieDetails

--- src/features/movieDetails/MovieDetails.js
+++ src/features/movieDetails/MovieDetails.js
@@ -1,6 +1,16 @@
-const MovieDetails = () => {
+import { BigPoster, MoviePage } from "../movieDetails/styled";
+
+const securyBaseUrl = "https://image.tmdb.org/t/p/";
+const bigPosterSize = "w500";
+
+const MovieDetails = (backdrop_path) => {
   return (
     <>
+      <MoviePage>
+        <BigPoster
+          src={backdrop_path ? `${securyBaseUrl}${bigPosterSize}${backdrop_path}` : null} alt="bigPoster"
+        />
+      </MoviePage>
       <p>Movie Details</p>
     </>
   );
