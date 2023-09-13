b294b8cae1d2a980173ae7c31a4e02aa275b7944
Piotr Michalski <pipi6620@gmail.com>
2023-09-13 20:30:34 +0200

    added fetchSearchMovies and its handler

--- src/features/moviesSaga.js
+++ src/features/moviesSaga.js
@@ -1,22 +1,38 @@
-import { takeEvery, call, put, all, delay } from "redux-saga/effects";
-import { fetchPopularMovies, fetchPopularMoviesError, setGenres, setMoviesData } from "./moviesSlice";
+import { takeEvery, call, put, all, delay, takeLatest } from "redux-saga/effects";
+import { fetchPopularMovies, fetchError, setGenres, setMoviesData, fetchSearchMovies } from "./moviesSlice";
 import { getPopularMovies } from "./getPopularMovies";
 import { getGenresList } from "./getGenresList";
+import { getSearchResults } from "../common/Navigation/Search/getSearchResults";
 
 function* fetchPopularMoviesHandler() {
     try {
         yield delay(1000);
-        const [ popularMovies, genres ] = yield all ([
+        const [popularMovies, genres] = yield all([
             call(getPopularMovies),
             call(getGenresList),
         ]);
         yield put(setMoviesData(popularMovies));
         yield put(setGenres(genres));
     } catch (error) {
-        yield put(fetchPopularMoviesError());
+        yield put(fetchError());
+    }
+}
+
+function* fetchSearchMoviesHandler({ payload: options }) {
+    try {
+        yield delay(1000);
+        const [popularMovies, genres] = yield all([
+            call(getSearchResults, options.query, options.page),
+            call(getGenresList),
+        ]);
+        yield put(setMoviesData(popularMovies));
+        yield put(setGenres(genres));
+    } catch (error) {
+        yield put(fetchError());
     }
 }
 
 export function* moviesSaga() {
     yield takeEvery(fetchPopularMovies.type, fetchPopularMoviesHandler);
+    yield takeLatest(fetchSearchMovies.type, fetchSearchMoviesHandler);
 }
