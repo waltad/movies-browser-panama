703878278df359a5bb4f0ee078c632cfde7480da
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-13 22:01:11 +0200

    added styled MovieSection

--- src/features/movieDetails/MovieDetails.js
+++ src/features/movieDetails/MovieDetails.js
@@ -1,15 +1,29 @@
-import { BigPoster, MoviePage } from "../movieDetails/styled";
+// import { useDispatch } from "react-redux";
+import { BigPoster, MoviePage, MovieSection, Section } from "../movieDetails/styled";
+// import { selectMovieDetails } from "../moviesSlice";
 
 const securyBaseUrl = "https://image.tmdb.org/t/p/";
 const bigPosterSize = "w500";
 
 const MovieDetails = (backdrop_path) => {
+  
+  // const dispatch = useDispatch();
+  // dispatch(selectMovieDetails);
+
   return (
     <>
       <MoviePage>
         <BigPoster
           src={backdrop_path ? `${securyBaseUrl}${bigPosterSize}${backdrop_path}` : null} alt="bigPoster"
         />
+        
+        <MovieSection>
+
+        </MovieSection>
+
+        <Section>
+
+        </Section>
       </MoviePage>
       <p>Movie Details</p>
     </>
