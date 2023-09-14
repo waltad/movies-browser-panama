9d9ace40e1782c9601c4ac3fe583db9746c8705c
Piotr Michalski <pipi6620@gmail.com>
2023-09-14 23:32:56 +0200

    removed margins from Wrapper

--- src/common/States/styled.js
+++ src/common/States/styled.js
@@ -8,10 +8,8 @@ export const Wrapper = styled.div`
     gap: 24px;
     padding: 0 20px;
     text-align: center;
-    margin-top: 56px;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
         gap: 14px;
-        margin-top: 24px;
     }
 `;
