a1c10e2b8a3b04eea0573f9e0904a209ea1516ce
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-14 21:58:16 +0200

    Added StyledPosterWithGradient component

--- src/features/movieDetails/MovieDetails.js
+++ src/features/movieDetails/MovieDetails.js
@@ -1,23 +1,27 @@
 // import { useDispatch } from "react-redux";
-import { BigPoster, MoviePage, MovieSection, Section } from "../movieDetails/styled";
+import { MoviePage, MovieSection, Section, StyledPosterWithGradient, Title } from "../movieDetails/styled";
 // import { selectMovieDetails } from "../moviesSlice";
 
-const securyBaseUrl = "https://image.tmdb.org/t/p/";
-const bigPosterSize = "w500";
+// const securyBaseUrl = "https://image.tmdb.org/t/p/";
+// const bigPosterSize = "w500";
 
 const MovieDetails = (backdrop_path) => {
   
   // const dispatch = useDispatch();
   // dispatch(selectMovieDetails);
-
+// src={backdrop_path ? `${securyBaseUrl}${bigPosterSize}${backdrop_path}` : null} alt="bigPoster"
   return (
     <>
       <MoviePage>
-        <BigPoster
-          src={backdrop_path ? `${securyBaseUrl}${bigPosterSize}${backdrop_path}` : null} alt="bigPoster"
-        />
+        <StyledPosterWithGradient>
+          <img src="https://image.tmdb.org/t/p/w500//5mzr6JZbrqnqD8rCEvPhuCE5Fw2.jpg" alt="bigPoster" />
+        </StyledPosterWithGradient>
         
         <MovieSection>
+          <Title>
+            Mulan
+          </Title>
+
 
         </MovieSection>
 
--- src/features/movieDetails/styled.js
+++ src/features/movieDetails/styled.js
@@ -3,27 +3,54 @@ import styled from "styled-components";
 export const MoviePage = styled.div`
   display: flex;
   flex-direction: column;
+  align-items:center;
   row-gap: 40px;
   width: 100%;
   background: ${({ theme }) => theme.colors.whisper};
 `;
 
-export const BigPoster = styled.img`
+export const StyledPosterWithGradient = styled.div`
+  background-image: linear-gradient(189.44deg, rgba(0, 0, 0, 0) 58.48%, rgba(0, 0, 0, 0.106473) 63.17%, rgba(0, 0, 0, 0.235359) 68.85%, rgba(0, 0, 0, 0.492821) 78.08%, rgba(0, 0, 0, 0.740286) 85.86%, #000000 92.87%),
+    linear-gradient(270deg, #000000 14.11%, rgba(0, 0, 0, 0.873268) 15.08%, rgba(0, 0, 0, 0.720529) 16.51%, rgba(0, 0, 0, 0.294577) 19.99%, rgba(0, 0, 0, 0.159921) 21.88%, rgba(0, 0, 0, 0) 25.68%),
+    linear-gradient(90deg, #000000 13.6%, rgba(0, 0, 0, 0.984059) 14.58%, rgba(0, 0, 0, 0.967732) 15.44%, rgba(0, 0, 0, 0.865569) 16.3%, rgba(0, 0, 0, 0.230315) 22.87%, rgba(0, 0, 0, 0) 26.64%),
+    linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.689555) 4.66%, rgba(0, 0, 0, 0.439033) 9.34%, rgba(0, 0, 0, 0.20628) 15.16%, rgba(0, 0, 0, 0) 24.22%);
   width: 100%;
   height: auto;
-  background: linear-gradient(189.44deg, rgba(0, 0, 0, 0) 58.48%, rgba(0, 0, 0, 0.106473) 63.17%, rgba(0, 0, 0, 0.235359) 68.85%, rgba(0, 0, 0, 0.492821) 78.08%, rgba(0, 0, 0, 0.740286) 85.86%, #000000 92.87%),
-    linear-gradient(90deg, #000000 13.6%, rgba(0, 0, 0, 0.984059) 14.58%, rgba(0, 0, 0, 0.967732) 15.44%, rgba(0, 0, 0, 0.865569) 16.3%, rgba(0, 0, 0, 0.230315) 22.87%, rgba(0, 0, 0, 0) 26.64%),
-    linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.689555) 4.66%, rgba(0, 0, 0, 0.439033) 9.34%, rgba(0, 0, 0, 0.20628) 15.16%, rgba(0, 0, 0, 0) 24.22%),
-    linear-gradient(270deg, #000000 14.11%, rgba(0, 0, 0, 0.873268) 15.08%, rgba(0, 0, 0, 0.720529) 16.51%, rgba(0, 0, 0, 0.294577) 19.99%, rgba(0, 0, 0, 0.159921) 21.88%, rgba(0, 0, 0, 0) 25.68%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
 
+// export const BigPoster = styled.img`
+//   width: 100%;
+//   height: auto;
+//   background: linear-gradient(189.44deg, rgba(0, 0, 0, 0) 58.48%, rgba(0, 0, 0, 0.106473) 63.17%, rgba(0, 0, 0, 0.235359) 68.85%, rgba(0, 0, 0, 0.492821) 78.08%, rgba(0, 0, 0, 0.740286) 85.86%, #000000 92.87%),
+//     linear-gradient(270deg, #000000 14.11%, rgba(0, 0, 0, 0.873268) 15.08%, rgba(0, 0, 0, 0.720529) 16.51%, rgba(0, 0, 0, 0.294577) 19.99%, rgba(0, 0, 0, 0.159921) 21.88%, rgba(0, 0, 0, 0) 25.68%),
+//     linear-gradient(90deg, #000000 13.6%, rgba(0, 0, 0, 0.984059) 14.58%, rgba(0, 0, 0, 0.967732) 15.44%, rgba(0, 0, 0, 0.865569) 16.3%, rgba(0, 0, 0, 0.230315) 22.87%, rgba(0, 0, 0, 0) 26.64%),
+//     linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.689555) 4.66%, rgba(0, 0, 0, 0.439033) 9.34%, rgba(0, 0, 0, 0.20628) 15.16%, rgba(0, 0, 0, 0) 24.22%);
+// `;
+
 export const Section = styled.section`
+  display:grid;
   width: 71%;
 `;
 
 export const MovieSection = styled(Section)`
   padding: 40px;
   background-color: ${({theme}) => theme.colors.white};
+`;
+
+export const Title = styled.div`
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 1.2;
+  text-align: left;
 `;
 
 /* Content */
