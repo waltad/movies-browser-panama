d27f8925e62235fc0f43cec27d645a8661f5b37b
Piotr Michalski <pipi6620@gmail.com>
2023-09-14 18:13:13 +0200

    added "type" to getSearchResults function

--- src/common/Navigation/Search/getSearchResults.js
+++ src/common/Navigation/Search/getSearchResults.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { authorizationKey } from '../../../features/authorizationKey';
 
-export const getSearchResults = async (query, page) => {
+export const getSearchResults = async (query, page, type) => {
     const options = {
         method: 'GET',
-        url: 'https://api.themoviedb.org/3/search/movie',
+        url: `https://api.themoviedb.org/3/search/${type}`,
         params: { query: query, include_adult: 'false', language: 'en-US', page: page },
         headers: {
             accept: 'application/json',
