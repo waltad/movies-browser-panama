260340b625173d631e169b376c2c50370a924cc3
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-16 01:08:01 +0200

    set align-self center for StyledStar

--- src/common/StyledStar/index.js
+++ src/common/StyledStar/index.js
@@ -4,7 +4,7 @@ import star from "../../images/Star.png";
 export default styled.img`
   width: 24px;
   height: 24px;
-  src: ${(props) => props.src = star};
+  align-self: center;
 
 @media (max-width: ${({ theme }) => theme.breakpoints}) {
   width: 16px;
