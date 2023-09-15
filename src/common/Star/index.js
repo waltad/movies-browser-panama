6d4c3a8773a52b76f10b2204dfabfa8bb82dabb8
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-09-15 13:12:21 +0200

    separated Star styled

---
+++ src/common/Star/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.img`
+width: 24px;
+height: 24px;
+
+@media (max-width: ${({ theme }) => theme.breakpoints}) {
+  width: 16px;
+  height: 16px;
+}
+`;
