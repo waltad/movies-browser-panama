a2c6ba00a515d94e0c98f842f61e651c4fd0d6aa
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-16 00:05:27 +0200

    rename Star to StyledStar

--- src/common/StyledStar/index.js
+++ src/common/StyledStar/index.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import star from "../../images/Star.png";
 
 export default styled.img`
-width: 24px;
-height: 24px;
+  width: 24px;
+  height: 24px;
+  src: ${(props) => props.src = star};
 
 @media (max-width: ${({ theme }) => theme.breakpoints}) {
   width: 16px;
