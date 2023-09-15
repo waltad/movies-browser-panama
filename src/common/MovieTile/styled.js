8ca9c57e6ac2e72b447e2618d4324aaa6ea581a1
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-09-15 12:28:52 +0200

    cut Title line and remove Genres

--- src/common/MovieTile/styled.js
+++ src/common/MovieTile/styled.js
@@ -11,7 +11,7 @@ export const Wrapper = styled.div`
   grid-template-columns: auto;
   grid-template-rows: auto auto auto;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     grid-template-columns: auto auto;
     grid-template-rows: 1fr 3fr;
     column-gap: 16px;
@@ -25,7 +25,7 @@ export const Poster = styled.img`
   height: 434px;
   border-radius: 5px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     width: 114px;
     height: 169px;
     grid-column: 1;
@@ -36,7 +36,7 @@ export const Poster = styled.img`
 export const MainInfo = styled.div`
   height: 134px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     height: auto;
     grid-column: 2;
     grid-row: 1;
@@ -49,10 +49,18 @@ export const Title = styled.div`
   line-height: 1.3;
   text-align: left;
   margin: 8px 0;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: 16px;
     margin: 0;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
   }
 `;
 
@@ -63,39 +71,8 @@ export const Year = styled.div`
   color: ${({ theme }) => theme.colors.waterloo};
   margin: 8px 0 0 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: 13px;
-  }
-`;
-
-export const Genres = styled.div`
-  width: 292px;
-  display: flex;
-  flex-wrap: wrap;
-  column-gap: 8px;
-  row-gap: 2px;
-  margin: 8px 0 0 0;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
-    max-width: 126px;
-    height: auto;
-  }
-`;
-
-export const Genre = styled.div`
-  height: 36px;
-  padding: 8px 16px 8px 16px;
-  border-radius: 5px;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 1.4;
-  text-align: left;
-  background-color: ${({ theme }) => theme.colors.mystic};
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
-    height: 19px;
-    padding: 4px 8px 4px 8px;
-    font-size: 10px;
   }
 `;
 
@@ -107,7 +84,7 @@ export const Rating = styled.div`
   height: 24px;
   margin: 16px 0 0 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     grid-column: 2;
     grid-row: 2;
     column-gap: 8px;
@@ -118,7 +95,7 @@ export const Star = styled.img`
   width: 24px;
   height: 24px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     width: 16px;
     height: 16px;
   }
@@ -129,7 +106,7 @@ export const Value = styled.div`
   font-weight: 600;
   line-height: 1.5;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: 13px;
   }
 `;
@@ -140,7 +117,7 @@ export const Votes = styled.div`
   line-height: 1.5;
   color: ${({ theme }) => theme.colors.waterloo};
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMini}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: 13px;
   }
 `;
