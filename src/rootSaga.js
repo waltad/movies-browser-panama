beb15f56f14603e9366cbb1371d28683acee07ae
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-16 21:53:41 +0200

    added movieDetails Saga to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import { moviesSaga } from "./features/moviesSaga";
 import { peopleSaga } from "./features/peopleSaga";
+import { movieDetailsSaga } from "./features/movieDetailsSaga";
 
 export default function* rootSaga() {
     yield all([
         moviesSaga(),
         peopleSaga(),
+        movieDetailsSaga(),
     ]);
 };
