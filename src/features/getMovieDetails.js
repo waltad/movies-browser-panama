20887ac034f8760d5350ad21ebb2fa541781a22a
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-16 21:52:24 +0200

    added getMovieDetails.js

---
+++ src/features/getMovieDetails.js
@@ -0,0 +1,28 @@
+// import { useSelector } from "react-redux";
+import { authorizationKey } from "./authorizationKey";
+// import { selectMovieId } from "./moviesSlice";
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: authorizationKey
+  }
+};
+
+
+
+export const getMovieDetails = async (movieId) => {
+  
+  // const movieId = useSelector(selectMovieId);
+  const urlMovieDetails = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
+  
+  const response = await fetch(urlMovieDetails, options);
+
+  if (!response.ok) {
+    new Error(response.statusText);
+  }
+
+  const result = await response.json();
+  return result;
+};
