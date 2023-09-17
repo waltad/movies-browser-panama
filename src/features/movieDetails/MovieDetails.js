3f4f2a8ebb5a2bb8ca443bf7c2820bb015636617
Tadek Waluga <tadek.waluga@gmail.com>
2023-09-17 15:48:37 +0200

    Added first interaction to MovieDetails view

--- src/features/movieDetails/MovieDetails.js
+++ src/features/movieDetails/MovieDetails.js
@@ -1,4 +1,6 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Genres from "../../common/Genres";
 import Genre from "../../common/Genre";
 import {
@@ -21,74 +23,91 @@ import noPoster from "../../images/Movie.png";
 import star from "../../images/Star.png";
 import StyledStar from "../../common/StyledStar";
 import { selectMovieId } from "../moviesSlice";
-import { selectMovieDetailsData } from "../movieDetailsSlice";
+import { fetchMovieDetails, selectMovieDetailsData, selectMovieDetailsStatus } from "../movieDetailsSlice";
+import { ErrorMessage } from "../../common/States/Error";
+import { LoadingIcon } from "../../common/States/Loading";
 
 const securyBaseUrl = "https://image.tmdb.org/t/p/";
 const bigPosterSize = "w500";
+const posterSize = "w300";
+// { backdrop_path, poster_path, title, release_date, vote_average, vote_count }
 
-const MovieDetails = ({ backdrop_path, poster_path, title, release_date, vote_average, vote_count }) => {
-  // const dispatch = useDispatch();
-  // dispatch(selectMovieDetails);
-  // src={backdrop_path ? `${securyBaseUrl}${bigPosterSize}${backdrop_path}` : null} alt="bigPoster"
-  return (
-    <>
-      <MoviePage>
-        <BigPosterWithGradient>
-          <img
-            src={backdrop_path ? `${securyBaseUrl}${bigPosterSize}${backdrop_path}` : null}
-            alt="bigPoster"
-          />
-        </BigPosterWithGradient>
+const MovieDetails = () => {
+  const dispatch = useDispatch();
+  const location = useLocation().pathname;
+  const movieId = (useSelector(selectMovieId)).toString();
+  const status = useSelector(selectMovieDetailsStatus);
+  const movieDetails = useSelector(selectMovieDetailsData);
 
-        <MovieSection>
-          
-          <Poster src={poster_path ? poster_path : noPoster}/>
+  useEffect(() => {
+    if (location.includes("movieDetails")) dispatch(fetchMovieDetails(movieId));
+  }, []);
 
-          <Info>
-            <Title>
-              Mulan
-              {title ? title : null}
-            </Title>
+  if (status === "error") {
+    return <ErrorMessage />;
+  }
 
-            <Year>
-              2020
-              {release_date ? release_date.slice(0, 4) : null}
-            </Year>
+  if (status === "loading") {
+    return <LoadingIcon />;
+  }
 
-            <ProductionAndRelease>
-              <StyledText>Production: </StyledText> <br />
-              <StyledText>Release date: </StyledText>
-            </ProductionAndRelease>
+  if (status === "success") {
+    return (
+      <>
+        <MoviePage>
+          <BigPosterWithGradient>
+            <img
+              src={movieDetails.backdrop_path ? `${securyBaseUrl}${posterSize}${movieDetails.backdrop_path}` : null}
+              alt="bigPoster"
+            />
+          </BigPosterWithGradient>
 
-            <Genres>
-              <Genre>Horror</Genre><Genre>Adventure</Genre><Genre>Science fiction</Genre>
-            </Genres>
+          <MovieSection>
 
-            <Rating>
-              <StyledStar src={star}/>
-              <Value>
-                7,8
-                {vote_average ? vote_average.toFixed(1) : null}
-              </Value>
-              <SmallerText>
-                / 10
-              </SmallerText>
-              <SmallerText>
-                {vote_count} votes
-              </SmallerText>
-            </Rating>
-            <Description>
-              A young Chinese maiden disguises herself as a male warrior in order to save her father. 
-              Disguises herself as a male warrior in order to save her father.  A young Chinese maiden disguises herself as a male warrior in order to save her father.
-            </Description>
-          </Info>
-        </MovieSection>
+            <Poster src={movieDetails.poster_path ? `${securyBaseUrl}${bigPosterSize}${movieDetails.poster_path}` : noPoster} />
 
-        <Section></Section>
-      </MoviePage>
-      <p>Movie Details</p>
-    </>
-  );
+            <Info>
+              <Title>
+                {movieDetails.title ? movieDetails.title : null}
+              </Title>
+
+              <Year>
+                {movieDetails.release_date ? movieDetails.release_date.slice(0, 4) : null}
+              </Year>
+
+              <ProductionAndRelease>
+                <StyledText>Production: </StyledText> <br />
+                <StyledText>Release date: </StyledText>
+              </ProductionAndRelease>
+
+              <Genres>
+                <Genre>Horror</Genre><Genre>Adventure</Genre><Genre>Science fiction</Genre>
+              </Genres>
+
+              <Rating>
+                <StyledStar src={star} />
+                <Value>
+                  {movieDetails.vote_average ? (movieDetails.vote_average).toFixed(1) : null}
+                </Value>
+                <SmallerText>
+                  / 10
+                </SmallerText>
+                <SmallerText>
+                  {movieDetails.vote_count} votes
+                </SmallerText>
+              </Rating>
+              <Description>
+                {movieDetails.overview}
+              </Description>
+            </Info>
+          </MovieSection>
+
+          <Section></Section>
+        </MoviePage>
+        <p>Movie Details</p>
+      </>
+    );
+  }
 };
 
 export default MovieDetails;
