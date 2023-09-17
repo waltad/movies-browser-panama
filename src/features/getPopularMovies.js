6e7974d1958c7ed0d3de5536b28aaf418df09717
Piotr Michalski <pipi6620@gmail.com>
2023-09-17 22:38:03 +0200

    adding page

--- src/features/getPopularMovies.js
+++ src/features/getPopularMovies.js
@@ -8,11 +8,9 @@ const options = {
   },
 };
 
-const urlPopularMovies = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+export const getPopularMovies = async (page) => {
 
-export const getPopularMovies = async () => {
-
-  const response = await fetch(urlPopularMovies, options);
+  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`, options);
 
   if (!response.ok) {
     new Error(response.statusText);
