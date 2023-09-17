1570a8dc9e0c97d5b39d5fcb36ab6b5aa9c10160
Piotr Michalski <pipi6620@gmail.com>
2023-09-17 22:35:46 +0200

    removed page from moviesSlice

--- src/features/moviesSlice.js
+++ src/features/moviesSlice.js
@@ -3,24 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const moviesSlice = createSlice({
     name: 'movies',
     initialState: {
-        page: 1,
         status: undefined,
         moviesData: [],
         genres: [],
     },
     reducers: {
-        nextPage: state => {
-            state.page = state.page + 1;
-        },
-        previousPage: state => {
-            state.page = state.page - 1;
-        },
-        setFirstPage: state => {
-            state.page = 1;
-        },
-        setLastPage: (state, { payload: totalPages }) => {
-            state.page = totalPages;
-        },
         fetchPopularMovies: state => {
             state.status = "loading";
         },
@@ -41,10 +28,6 @@ const moviesSlice = createSlice({
 });
 
 export const {
-    nextPage,
-    previousPage,
-    setFirstPage,
-    setLastPage,
     fetchPopularMovies,
     setMoviesData,
     fetchError,
@@ -53,7 +36,6 @@ export const {
 } = moviesSlice.actions;
 
 export const selectMoviesState = state => state.movies;
-export const selectPage = state => selectMoviesState(state).page;
 export const selectStatus = state => selectMoviesState(state).status;
 export const selectData = state => selectMoviesState(state).moviesData;
 export const selectGenres = state => selectMoviesState(state).genres;
