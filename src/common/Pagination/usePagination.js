e33f79d607bd73b023322b31493efcbe9a0ce6bc
Piotr Michalski <pipi6620@gmail.com>
2023-09-17 22:36:43 +0200

    usePagination hook created

---
+++ src/common/Pagination/usePagination.js
@@ -0,0 +1,50 @@
+import { useQueryParameter, useReplaceQueryParameter } from "../Navigation/queryParameters";
+import paginationParamName from "./paginationParamName";
+import { useState, useEffect } from "react";
+import { selectData } from "../../features/moviesSlice";
+import { selectPeopleList } from "../../features/peopleSlice";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector } from "react-redux";
+
+export const usePagination = () => {
+    const location = useLocation();
+    const moviesData = useSelector(selectData);
+    const peopleData = useSelector(selectPeopleList);
+    const data = location.pathname.startsWith("/movies")
+        ? moviesData : peopleData;
+    const totalPages = data.total_pages > 500 ? 500 : data.total_pages;
+    const replaceQueryParameter = useReplaceQueryParameter();
+    const [page, setPage] = useState(parseInt(useQueryParameter(paginationParamName)) || 1);
+
+    useEffect(() => {
+        replaceQueryParameter({
+            key: paginationParamName,
+            value: page,
+        });
+    }, [page, replaceQueryParameter])
+
+    const setNextPage = () => {
+        setPage((page) => page += 1);
+    };
+
+    const setPreviousPage = () => {
+        setPage((page) => page -= 1);
+    };
+
+    const setFirstPage = () => {
+        setPage(1);
+    };
+
+    const setLastPage = () => {
+        setPage(totalPages);
+    };
+
+    return {
+        page,
+        totalPages,
+        setNextPage,
+        setPreviousPage,
+        setFirstPage,
+        setLastPage
+    };
+};
