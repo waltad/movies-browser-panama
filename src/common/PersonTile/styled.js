c00ff89d82bba85e7ea2de93c2834b7608fc4532
Piotr Michalski <pipi6620@gmail.com>
2023-09-17 22:41:38 +0200

    changed long names to be only 2 lines long

--- src/common/PersonTile/styled.js
+++ src/common/PersonTile/styled.js
@@ -41,6 +41,11 @@ export const Name = styled.header`
     font-size: 22px;
     font-weight: 500;
     line-height: 1.3;
+    overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	-webkit-line-clamp: 2;
+	-webkit-box-orient: vertical;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
         font-size: 14px;
