4fd167ec9c02bb031c41e37e893cd09fddd65b9f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-09-19 01:01:28 +0100

    getPersonById added

--- src/features/personDetails/getPersonById.js
+++ src/features/personDetails/getPersonById.js
@@ -1,2 +1,17 @@
-import { APIkey, personAPI  } from "../../apiURL";
+import { APIkey, personAPI } from "../../apiURL";
 
+export const getPersonById = async (id) => {
+  try {
+    const response = await fetch(
+      `${personAPI}${id}?api_key=${APIkey}&language=en-US`
+    );
+    if (!response.ok) {
+      throw new Error("An error occurred. Please try again later.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+// This function is very similar to the getPeople function. The only difference is that we are now passing in an id as an argument. We then use that id to fetch the data for a specific person. The data is then returned.
