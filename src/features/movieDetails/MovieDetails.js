32a870a94cc47e505dca028bc0a6b210bbbb4287
Piotr Michalski <pipi6620@gmail.com>
2023-09-20 12:51:06 +0200

    using switch instead of if statements, small fixes

--- src/features/movieDetails/MovieDetails.js
+++ src/features/movieDetails/MovieDetails.js
@@ -47,97 +47,126 @@ const MovieDetails = () => {
   const movieDetails = useSelector(selectMovieDetailsData);
 
   useEffect(() => {
-    if (location.includes("movieDetails")) dispatch(fetchMovieDetails(id));
+    if (location.includes("movies/")) dispatch(fetchMovieDetails(id));
   }, [dispatch, location, id]);
 
-  if (status === "error") {
-    return <ErrorMessage />;
-  }
-
-  if (status === "loading") {
-    return <LoadingIcon />;
-  }
-
-  if (status === "success") {
-    return (
-      <>
-        <MoviePage>
-          <BigPosterWithGradient>
-            <img
-              src={movieDetails.backdrop_path ? `${securyBaseUrl}${bigPosterSize}${movieDetails.backdrop_path}` : null}
-              alt="bigPoster"
-            />
-            <MainInfo>
-              <BigTitle>
-                {movieDetails.title ? movieDetails.title : null}
-              </BigTitle>
-              <TopRating>
-                <TopStar>
-                  <img src={star} alt="star"/>
-                </TopStar>
-                <TopValue>
-                  {movieDetails.vote_average ? (movieDetails.vote_average).toFixed(1) : null}
-                  <TopSmallerText> /10</TopSmallerText>
-                </TopValue>
-              </TopRating>
-              <TopVotes>
-               {movieDetails.vote_count ? movieDetails.vote_count : null} votes
-              </TopVotes>
-            </MainInfo>
-          </BigPosterWithGradient>
-
-          <MovieSection>
-
-            <Poster src={movieDetails.poster_path ? `${securyBaseUrl}${posterSize}${movieDetails.poster_path}` : noPoster} />
-
-            <Info>
-              <Title>
-                {movieDetails.title ? movieDetails.title : null}
-              </Title>
-
-              <Year>
-                {movieDetails.release_date ? movieDetails.release_date.slice(0, 4) : null}
-              </Year>
-
-              <ProductionAndRelease>
-                <StyledText>Production: </StyledText>
-                {movieDetails.production_countries ? (movieDetails.production_countries).map((country) => (<span>{country.name}, </span>)) : null}<br />
-                <StyledText>Release date: </StyledText>
-                {movieDetails.release_date ? `${(movieDetails.release_date).slice(8, 10)}.${(movieDetails.release_date).slice(5, 7)}.${(movieDetails.release_date).slice(0, 4)}` : null}
-              </ProductionAndRelease>
-
-              <Genres>
-                {movieDetails.genres ? (movieDetails.genres).map((genre) => (<Genre>{genre.name}</Genre>)) : null}
-              </Genres>
-
-              <Rating>
-                <StyledStar>
-                  <img src={star} alt="star" />
-                </StyledStar>
-                
-                <Value>
-                  {movieDetails.vote_average ? (movieDetails.vote_average).toFixed(1) : null}
-                </Value>
-                
-                <SmallerText>
-                  / 10
-                </SmallerText>
-                
-                <Votes>
+  switch (status) {
+    case "error":
+      return (
+        <ErrorMessage />
+      );
+    case "loading":
+      return (
+        <LoadingIcon />
+      );
+    case "success":
+      return movieDetails.status_message ? (
+        <ErrorMessage />
+      ) : (
+        <>
+          <MoviePage>
+            <BigPosterWithGradient>
+              <img
+                src={movieDetails.backdrop_path ?
+                  `${securyBaseUrl}${bigPosterSize}${movieDetails.backdrop_path}` : null
+                }
+                alt="bigPoster"
+              />
+              <MainInfo>
+                <BigTitle>
+                  {movieDetails.title ? movieDetails.title : null}
+                </BigTitle>
+                <TopRating>
+                  <TopStar>
+                    <img src={star} alt="star" />
+                  </TopStar>
+                  <TopValue>
+                    {movieDetails.vote_average ?
+                      (movieDetails.vote_average).toFixed(1) : null
+                    }
+                    <TopSmallerText> /10</TopSmallerText>
+                  </TopValue>
+                </TopRating>
+                <TopVotes>
                   {movieDetails.vote_count ? movieDetails.vote_count : null} votes
-                </Votes>
-              </Rating>
-            </Info>
+                </TopVotes>
+              </MainInfo>
+            </BigPosterWithGradient>
 
-            <Description>
+            <MovieSection>
+
+              <Poster src={movieDetails.poster_path ?
+                `${securyBaseUrl}${posterSize}${movieDetails.poster_path}` : noPoster
+              } />
+
+              <Info>
+                <Title>
+                  {movieDetails.title ? movieDetails.title : null}
+                </Title>
+
+                <Year>
+                  {movieDetails.release_date ?
+                    movieDetails.release_date.slice(0, 4) : null
+                  }
+                </Year>
+
+                <ProductionAndRelease>
+                  <StyledText>Production: </StyledText>
+                  {movieDetails.production_countries ?
+                    (movieDetails.production_countries).map((country) => (
+                      <span key={country.name}>{country.name}, </span>
+                    )) : null
+                  }
+                  <br />
+                  <StyledText>Release date: </StyledText>
+                  {movieDetails.release_date ?
+                    `${(movieDetails.release_date).slice(8, 10)}.${(movieDetails.release_date).slice(5, 7)}.${(movieDetails.release_date).slice(0, 4)}`
+                    : null
+                  }
+                </ProductionAndRelease>
+
+                <Genres>
+                  {movieDetails.genres ?
+                    (movieDetails.genres).map((genre) => (
+                      <Genre>{genre.name}</Genre>
+                    )) : null
+                  }
+                </Genres>
+
+                <Rating>
+                  <StyledStar>
+                    <img src={star} alt="star" />
+                  </StyledStar>
+
+                  <Value>
+                    {movieDetails.vote_average ?
+                      (movieDetails.vote_average).toFixed(1) : null
+                    }
+                  </Value>
+
+                  <SmallerText>
+                    / 10
+                  </SmallerText>
+
+                  <Votes>
+                    {movieDetails.vote_count ? movieDetails.vote_count : null} votes
+                  </Votes>
+                </Rating>
+              </Info>
+
+              <Description>
                 {movieDetails.overview ? movieDetails.overview : null}
               </Description>
-          </MovieSection>
+            </MovieSection>
 
-          <Section></Section>
-        </MoviePage>
-      </>
-    );
+            <Section></Section>
+          </MoviePage>
+        </>
+      );
+    default:
+      return (
+        <ErrorMessage />
+      );
   }
 };
 
