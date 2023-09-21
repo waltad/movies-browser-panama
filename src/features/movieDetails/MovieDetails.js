b2c39676585a58c3a746993fa9d0e12db30c00d3
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-09-21 10:16:34 +0200

    renamed: PeopleList to CreditsList, id to movieId

--- src/features/movieDetails/MovieDetails.js
+++ src/features/movieDetails/MovieDetails.js
@@ -25,7 +25,7 @@ import {
   TopSmallerText,
   TopVotes,
   SmallerText,
-  PeopleList,
+  Creditsist,
   StyledNavLink,
   Section,
 } from "../movieDetails/styled";
@@ -33,7 +33,12 @@ import noPoster from "../../images/Movie.png";
 import star from "../../images/Star.png";
 import StyledStar from "../../common/StyledStar";
 import { PersonTile } from "../../common/PersonTile";
-import { fetchMovieDetails, selectMovieCreditsData, selectMovieDetailsData, selectMovieDetailsStatus } from "../movieDetailsSlice";
+import {
+  fetchMovieDetails,
+  selectMovieCreditsData,
+  selectMovieDetailsData,
+  selectMovieDetailsStatus,
+} from "../movieDetailsSlice";
 import { ErrorMessage } from "../../common/States/Error";
 import { LoadingIcon } from "../../common/States/Loading";
 import { useParams } from "react-router-dom";
@@ -43,7 +48,7 @@ const bigPosterSize = "original";
 const posterSize = "w500";
 
 const MovieDetails = () => {
-  const id = useParams().id;
+  const movieId = useParams().id;
   const dispatch = useDispatch();
   const location = useLocation().pathname;
   const status = useSelector(selectMovieDetailsStatus);
@@ -53,18 +58,14 @@ const MovieDetails = () => {
 
   useEffect(() => {
     if (location.includes("movies/"))
-      dispatch(fetchMovieDetails(id));
-  }, [dispatch, location, id]);
+    dispatch(fetchMovieDetails(movieId));
+  }, [dispatch, location, movieId]);
 
   switch (status) {
     case "error":
-      return (
-        <ErrorMessage />
-      );
+      return <ErrorMessage />;
     case "loading":
-      return (
-        <LoadingIcon />
-      );
+      return <LoadingIcon />;
     case "success":
       return movieDetails.status_message ? (
         <ErrorMessage />
@@ -73,70 +74,88 @@ const MovieDetails = () => {
           <MoviePage>
             <BigPosterWithGradient>
               <img
-                src={movieDetails.backdrop_path ?
-                  `${securyBaseUrl}${bigPosterSize}${movieDetails.backdrop_path}` : null
+                src={
+                  movieDetails.backdrop_path
+                    ? `${securyBaseUrl}${bigPosterSize}${movieDetails.backdrop_path}`
+                    : null
                 }
                 alt="bigPoster"
               />
               <MainInfo>
                 <BigTitle>
-                  {movieDetails.title ? movieDetails.title : null}
+                  {movieDetails.title
+                    ? movieDetails.title
+                    : null}
                 </BigTitle>
+
                 <TopRating>
                   <TopStar>
                     <img src={star} alt="star" />
                   </TopStar>
+
                   <TopValue>
-                    {movieDetails.vote_average ?
-                      (movieDetails.vote_average).toFixed(1) : null
-                    }
+                    {movieDetails.vote_average
+                      ? movieDetails.vote_average.toFixed(1)
+                      : null}
                     <TopSmallerText> /10</TopSmallerText>
                   </TopValue>
+
                 </TopRating>
+
                 <TopVotes>
-                  {movieDetails.vote_count ? movieDetails.vote_count : null} votes
+                  {movieDetails.vote_count
+                    ? movieDetails.vote_count
+                    : null}{" "}
+                  votes
                 </TopVotes>
               </MainInfo>
             </BigPosterWithGradient>
 
             <MovieSection>
-
-              <Poster src={movieDetails.poster_path ?
-                `${securyBaseUrl}${posterSize}${movieDetails.poster_path}` : noPoster
-              } />
+              <Poster
+                src={
+                  movieDetails.poster_path
+                    ? `${securyBaseUrl}${posterSize}${movieDetails.poster_path}`
+                    : noPoster
+                }
+              />
 
               <Info>
                 <Title>
-                  {movieDetails.title ? movieDetails.title : null}
+                  {movieDetails.title
+                    ? movieDetails.title
+                    : null}
                 </Title>
 
                 <Year>
-                  {movieDetails.release_date ?
-                    movieDetails.release_date.slice(0, 4) : null
-                  }
+                  {movieDetails.release_date
+                    ? movieDetails.release_date.slice(0, 4)
+                    : null}
                 </Year>
 
                 <ProductionAndRelease>
                   <StyledText>Production: </StyledText>
-                  {movieDetails.production_countries ?
-                    (movieDetails.production_countries).map((country) => (
-                      <span key={country.name}>{country.name}, </span>
-                    )) : null
-                  }
+                  {movieDetails.production_countries
+                    ? movieDetails.production_countries.map((country) => (
+                        <span key={country.name}>{country.name}, </span>
+                      ))
+                    : null}
                   <br />
+
                   <StyledText>Release date: </StyledText>
-                  {movieDetails.release_date ?
-                    `${(movieDetails.release_date).slice(8, 10)}.${(movieDetails.release_date).slice(5, 7)}.${(movieDetails.release_date).slice(0, 4)}`
-                    : null
-                  }
+                  {movieDetails.release_date
+                    ? `${movieDetails.release_date.slice(8, 10)
+                      }.${movieDetails.release_date.slice(5, 7)
+                      }.${movieDetails.release_date.slice(0, 4)}`
+                    : null}
                 </ProductionAndRelease>
 
                 <Genres>
-                  {movieDetails.genres ?
-                    (movieDetails.genres).map((genre) => (
-                      <Genre key={genre.name}>{genre.name}</Genre>
-                    )) : null
-                  }
+                  {movieDetails.genres
+                    ? movieDetails.genres.map((genre) => (
+                        <Genre key={genre.name}>{genre.name}</Genre>
+                      ))
+                    : null}
                 </Genres>
 
                 <Rating>
@@ -145,66 +164,67 @@ const MovieDetails = () => {
                   </StyledStar>
 
                   <Value>
-                    {movieDetails.vote_average ?
-                      (movieDetails.vote_average).toFixed(1) : null
-                    }
+                    {movieDetails.vote_average
+                      ? movieDetails.vote_average.toFixed(1)
+                      : null}
                   </Value>
 
-                  <SmallerText>
-                    / 10
-                  </SmallerText>
+                  <SmallerText>/ 10</SmallerText>
 
                   <Votes>
-                    {movieDetails.vote_count ? movieDetails.vote_count : null} votes
+                    {movieDetails.vote_count
+                      ? movieDetails.vote_count
+                      : null}{" "}
+                    votes
                   </Votes>
                 </Rating>
               </Info>
 
               <Description>
-                {movieDetails.overview ? movieDetails.overview : null}
+                {movieDetails.overview
+                  ? movieDetails.overview
+                  : null}
               </Description>
             </MovieSection>
 
             <Section>
-              <Title>Cast</Title>
+              <Title credits>Cast</Title>
+
+              <Creditsist>
+                {cast.map((actor) => (
+                  <StyledNavLink key={actor.id} to="/personDetails">
+                    <PersonTile
+                      name={actor.name}
+                      profile_path={actor.profile_path}
+                      id={actor.id}
+                      additionalContent={actor.character}
+                    />
+                  </StyledNavLink>
+                ))}
+              </Creditsist>
             </Section>
-            
-            <PeopleList>
-              {cast.map((actor) => (
-                <StyledNavLink key={actor.id} to="/personDetails">
-                  <PersonTile
-                    name={actor.name}
-                    profile_path={actor.profile_path}
-                    id={actor.id}
-                    additionalContent={actor.character}
-                  />
-                </StyledNavLink>
-              ))}
-            </PeopleList>
 
             <Section>
-              <Title>Crew</Title>
+              <Title credits>Crew</Title>
+
+              <Creditsist>
+                {crew.map((person) => (
+                  <StyledNavLink key={person.id} to="/personDetails">
+                    <PersonTile
+                      name={person.name}
+                      profile_path={person.profile_path}
+                      id={person.id}
+                      additionalContent={person.job}
+                    />
+                  </StyledNavLink>
+                ))}
+              </Creditsist>
             </Section>
-            
-            <PeopleList>
-              {crew.map((person) => (
-                <StyledNavLink key={person.id} to="/personDetails">
-                  <PersonTile
-                    name={person.name}
-                    profile_path={person.profile_path}
-                    id={person.id}
-                    additionalContent={person.job}
-                  />
-                </StyledNavLink>
-              ))}
-            </PeopleList>
           </MoviePage>
         </>
       );
     default:
-      return (
-        <ErrorMessage />
-      );
+      return <ErrorMessage />;
   }
 };
 
