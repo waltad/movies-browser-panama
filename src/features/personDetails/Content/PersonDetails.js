65d13e41139e2d5a1f07251ab28806a6bd155f51
Piotr Michalski <pipi6620@gmail.com>
2023-09-25 15:10:13 +0200

    formatted code after merge

--- src/features/personDetails/Content/PersonDetails.js
+++ src/features/personDetails/Content/PersonDetails.js
@@ -50,46 +50,46 @@ const PersonDetails = () => {
               birthplace={personInfo.personDescription.place_of_birth}
               biography={personInfo.personDescription.biography}
             />
-                <>
-          <CastWrapper>
-            <Title>
-              Movies - cast {`(${personInfo.personMovies.cast.length})`}
-            </Title>
-            <List>
-              {personInfo.personMovies.cast.map((movie) => (
-                <StyledNavLink key={movie.id} to={`/movies/${movie.id}`}>
-                  <MovieTile
-                    poster_path={movie.poster_path}
-                    title={movie.original_title}
-                    genre_ids={(movie.genre_ids).slice(0, 2)}
-                    release_date={movie.release_date}
-                    vote_average={movie.vote_average}
-                    vote_count={movie.vote_count}
-                  />
-                </StyledNavLink>
-              ))}
-            </List>
-          </CastWrapper>
-          <CrewWrapper>
-            <Title>
-              Movies - crew {`(${personInfo.personMovies.crew.length})`}
-            </Title>
-            <List>
-              {personInfo.personMovies.crew.map((movie) => (
-                <StyledNavLink key={movie.id} to={`/movies/${movie.id}`}>
-                  <MovieTile
-                    poster_path={movie.poster_path}
-                    title={movie.original_title}
-                    genre_ids={(movie.genre_ids).slice(0, 2)}
-                    release_date={movie.release_date}
-                    vote_average={movie.vote_average}
-                    vote_count={movie.vote_count}
-                  />
-                </StyledNavLink>
-              ))}
-            </List>
-          </CrewWrapper>
-        </>
+            <>
+              <CastWrapper>
+                <Title>
+                  Movies - cast {`(${personInfo.personMovies.cast.length})`}
+                </Title>
+                <List>
+                  {personInfo.personMovies.cast.map((movie) => (
+                    <StyledNavLink key={movie.id} to={`/movies/${movie.id}`}>
+                      <MovieTile
+                        poster_path={movie.poster_path}
+                        title={movie.original_title}
+                        genre_ids={(movie.genre_ids).slice(0, 2)}
+                        release_date={movie.release_date}
+                        vote_average={movie.vote_average}
+                        vote_count={movie.vote_count}
+                      />
+                    </StyledNavLink>
+                  ))}
+                </List>
+              </CastWrapper>
+              <CrewWrapper>
+                <Title>
+                  Movies - crew {`(${personInfo.personMovies.crew.length})`}
+                </Title>
+                <List>
+                  {personInfo.personMovies.crew.map((movie) => (
+                    <StyledNavLink key={movie.id} to={`/movies/${movie.id}`}>
+                      <MovieTile
+                        poster_path={movie.poster_path}
+                        title={movie.original_title}
+                        genre_ids={(movie.genre_ids).slice(0, 2)}
+                        release_date={movie.release_date}
+                        vote_average={movie.vote_average}
+                        vote_count={movie.vote_count}
+                      />
+                    </StyledNavLink>
+                  ))}
+                </List>
+              </CrewWrapper>
+            </>
           </Wrapper>
         )
     default:
