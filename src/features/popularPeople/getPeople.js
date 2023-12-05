9a2e3607885c11e4e906b2764b0ac7c91d4a36d9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-12-05 01:08:42 +0000

    getPeople api method changed

--- src/features/popularPeople/getPeople.js
+++ src/features/popularPeople/getPeople.js
@@ -1,16 +1,48 @@
-import { APIkey, peopleUrl } from "../../apiURL";
+import { authorizationKey } from "../../authorizationKey";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: authorizationKey,
+  },
+};
 
 export const getPeople = async (page) => {
   try {
     const response = await fetch(
-      `${peopleUrl}?api_key=${APIkey}&language=en-US&page=${page}`
+      `https://api.themoviedb.org/3/person/popular?language=en-US&page=${page}`,
+      options
     );
+
     if (!response.ok) {
-      throw new Error("An error occurred. Please try again later.");
+      new Error(response.statusText);
     }
-    const data = await response.json();
-    return data;
+
+    const result = await response.json();
+    return result;
   } catch (error) {
     console.error(error);
+    throw error;
   }
 };
+
+// import { APIkey, peopleUrl } from "../../apiURL";
+
+// export const getPeople = async (page) => {
+//   try {
+//     const response = await fetch(
+//       `${peopleUrl}?api_key=${APIkey}&language=en-US&page=${page}`
+//     );
+
+//     if (!response.ok) {
+//       throw new Error("An error occurred. Please try again later.");
+//     }
+
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
